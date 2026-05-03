57afa546d5677d0dc5fa1ae5386102404f6b0abf
naraphan13 <thongnit2201@gmail.com>
2026-05-03 17:45:58 +0700

    open30

--- server.js
+++ server.js
@@ -51,5 +51,5 @@ app.use("/v1/lungsang-invoices", lungsangInvoicesRouter);
 app.use(notFound);
 app.use(errorMiddleware);
 
-const port = process.env.PORT || 9989;
+const port = process.env.PORT || 9988;
 app.listen(port, () => console.log(`✅ Server running on port ${port}`));
